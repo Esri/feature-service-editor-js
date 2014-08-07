210d6b8b443dcbace53b46469f64cb5abe5ffe44
Andy Gup <agup2001@msn.com>
2014-08-07 13:35:57 -0600

    deprecated featureEditor.utils.addRemoveColumns(). Moved to utils.js

--- libs/utils.js
+++ libs/utils.js
@@ -1,4 +1,14 @@
 /**
+ * Create our internal namespace
+ * @type {{utils: {dataGrid: {}}}}
+ */
+this.F = {
+    utils:{
+        dataGrid:{}
+    }
+}
+
+/**
  * Validates a url
  * @param url
  * @returns boolean
@@ -16,4 +26,23 @@ deleteDirtyBits = function(dataGrid){
     var dirty = dataGrid.dirty;
     var id = Object.keys(dirty)[0];
     delete dirty[id];
+}
+
+/**
+ * Helper function for adding and removing columns for better visibility
+ * in FeatureServices that have many fields.
+ * @param id
+ */
+addRemoveColumns = function(/* number */ id){
+
+    var column = F.utils.dataGrid.columns[id];
+
+    if(column.hidden == false){
+        F.utils.dataGrid.styleColumn(id,"display:none;");
+        F.utils.dataGrid.columns[id].hidden = true;
+    }
+    else{
+        F.utils.dataGrid.styleColumn(id,"display:table-cell;");
+        F.utils.dataGrid.columns[id].hidden = false;
+    }
 }

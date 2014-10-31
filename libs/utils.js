3af79c30fc347778d4b511cf2c1090f7104e9145
Andy Gup <agup2001@msn.com>
2014-10-31 14:18:03 -0600

    remove F namespace

--- libs/utils.js
+++ libs/utils.js
@@ -1,14 +1,4 @@
 /**
- * Create our internal namespace
- * @type {{utils: {dataGrid: {}}}}
- */
-this.F = {
-    utils:{
-        dataGrid:{}
-    }
-}
-
-/**
  * Validates a url
  * @param url
  * @returns boolean
@@ -32,24 +22,27 @@ deleteDirtyBits = function(dataGrid){
  * Helper function for adding and removing columns for better visibility
  * in FeatureServices that have many fields.
  * @param id
+ * @param grid
  */
-addRemoveColumns = function(/* number */ id){
+addRemoveColumns = function(/* number */ id,grid){
 
-    var column = F.utils.dataGrid.columns[id];
+    var column = grid.columns[id];
 
     if(column.hidden == false){
-        F.utils.dataGrid.styleColumn(id,"display:none;");
-        F.utils.dataGrid.columns[id].hidden = true;
+        grid.styleColumn(id,"display:none;");
+        grid.columns[id].hidden = true;
     }
     else{
-        F.utils.dataGrid.styleColumn(id,"display:table-cell;");
-        F.utils.dataGrid.columns[id].hidden = false;
+        grid.styleColumn(id,"display:table-cell;");
+        grid.columns[id].hidden = false;
     }
 }
 
-getFieldNamesArr = function(fields,xfield,yfield,callback){
+getFieldNamesArr = function(featureEditor,callback){
 
     var editableDetails = [];
+
+    var fields = featureEditor.featureLayer.fields;
 
     //Create a simple array of field names that are editable
     for(var item in fields){
@@ -60,8 +53,19 @@ getFieldNamesArr = function(fields,xfield,yfield,callback){
             var lcName = name.toLowerCase();
             //Get the names of the fields corresponding to x and y.
             //This could be problematic if someone uses x or y in the first part of the field name
-            if(lcName.indexOf("x") != -1 && lcName.substring(0,1) == "x") xField = name;
-            if(lcName.indexOf("y") != -1 && lcName.substring(0,1) == "y") yField = name;
+            if(lcName.indexOf("x") != -1 && lcName.substring(0,1) == "x") featureEditor.xField = name;
+            if(lcName.indexOf("y") != -1 && lcName.substring(0,1) == "y") featureEditor.yField = name;
+
+            // If there is no x or y field then look for lat lon
+            if(featureEditor.xfield == null || featureEditor.yfield == null){
+                if(lcName.indexOf("lat") != -1) featureEditor.xField = name;
+                if(lcName.indexOf("lon") != -1){
+                    featureEditor.yField = name;
+                }
+                else if(lcName.indexOf("lng") != -1){
+                    featureEditor.yField = name;
+                }
+            }
 
             if(editable == true){
                 editableDetails.push(name);
